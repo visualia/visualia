92a447392347cc7eaf5fc7a479f121b661e87f84
kristjanjansen <kristjan.jansen@gmail.com>
2021-06-13 18:37:04 +0300

    feat: better packaging

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
+import path from "path";
 import vue from "@vitejs/plugin-vue";
+import dts from "vite-dts";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
+  resolve: { dedupe: ["vue"] },
   build: {
     lib: {
-      entry: "./src/index.ts",
+      entry: path.resolve(__dirname, "./src/index.ts"),
       name: "visualia",
     },
     rollupOptions: {
       external: ["vue"],
       output: {
+        sourcemapExcludeSources: true,
         globals: {
           vue: "Vue",
         },
       },
     },
+    sourcemap: true,
+    target: "esnext",
   },
 });
