39cc1f2727c3f00a5d0daa23eddfc2de5e1f4706
kristjanjansen <kristjan.jansen@gmail.com>
2021-06-14 14:38:53 +0300

    alt work

---
+++ src/TestSlider.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import { inject } from "vue";
+const state = inject("state") as any;
+</script>
+
+<template><input type="range" v-model="state.x" /> {{ state.x }}</template>
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,40 @@
-import { Plugin } from "vue";
-import * as utils from "./utils";
-export { utils };
-export * from "./utils";
+import { Plugin, reactive } from "vue";
+// import * as utils from "./utils";
+// export { utils };
+// export * from "./utils";
 
-//@TODO Property 'glob' does not exist on type 'ImportMeta'.
-//@ts-ignore
-const componentFiles: Record<string, ComponentOptions> = import.meta.globEager(
-  "./components/**/*.vue"
-);
+// //@TODO Property 'glob' does not exist on type 'ImportMeta'.
+// //@ts-ignore
+// const componentFiles: Record<string, ComponentOptions> = import.meta.globEager(
+//   "./components/**/*.vue"
+// );
 
-export const components = Object.fromEntries(
-  Object.entries(componentFiles).map(([path, component]) => {
-    return [
-      path.split("/").slice(-1)[0].replace(".vue", ""),
-      component.default,
-    ];
-  })
-);
+// export const components = Object.fromEntries(
+//   Object.entries(componentFiles).map(([path, component]) => {
+//     return [
+//       path.split("/").slice(-1)[0].replace(".vue", ""),
+//       component.default,
+//     ];
+//   })
+// );
 
+// export const Visualia: Plugin = {
+//   install: (app) => {
+//     Object.entries(components).forEach(([name, component]) => {
+//       app.component(name, component);
+//     });
+//     Object.entries(utils).forEach(
+//       ([name, util]) => (app.config.globalProperties[name] = util)
+//     );
+//   },
+// };
+
+import TestSlider from "./TestSlider.vue";
 export const Visualia: Plugin = {
   install: (app) => {
-    Object.entries(components).forEach(([name, component]) => {
-      app.component(name, component);
-    });
-    Object.entries(utils).forEach(
-      ([name, util]) => (app.config.globalProperties[name] = util)
-    );
+    const state = reactive({ x: 0 });
+    app.provide("state", state);
+    app.component("TestSlider", TestSlider);
+    app.config.globalProperties.get = () => state.x;
   },
 };
