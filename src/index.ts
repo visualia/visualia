5cf72c42972d35747247ddcffd05eea8fe7ed2de
kristjanjansen <kristjan.jansen@gmail.com>
2021-06-14 11:47:31 +0300

    rollback

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Plugin, nextTick } from "vue";
+import { Plugin } from "vue";
 import * as utils from "./utils";
 export { utils };
 export * from "./utils";
@@ -20,13 +20,11 @@ export const components = Object.fromEntries(
 
 export const Visualia: Plugin = {
   install: (app) => {
-    nextTick(() => {
-      Object.entries(components).forEach(([name, component]) => {
-        app.component(name, component);
-      });
-      Object.entries(utils).forEach(
-        ([name, util]) => (app.config.globalProperties[name] = util)
-      );
+    Object.entries(components).forEach(([name, component]) => {
+      app.component(name, component);
     });
+    Object.entries(utils).forEach(
+      ([name, util]) => (app.config.globalProperties[name] = util)
+    );
   },
 };
