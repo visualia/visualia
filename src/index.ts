2f0078e13b707c439c602d99ba68fedcab9daab6
kristjanjansen <kristjan.jansen@gmail.com>
2021-06-14 15:47:49 +0300

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,31 @@
 import { Plugin, reactive } from "vue";
-// import * as utils from "./utils";
-// export { utils };
-// export * from "./utils";
+import * as utils from "./utils";
+export { utils };
+export * from "./utils";
 
-// //@TODO Property 'glob' does not exist on type 'ImportMeta'.
-// //@ts-ignore
-// const componentFiles: Record<string, ComponentOptions> = import.meta.globEager(
-//   "./components/**/*.vue"
-// );
+//@TODO Property 'glob' does not exist on type 'ImportMeta'.
+//@ts-ignore
+const componentFiles: Record<string, ComponentOptions> = import.meta.globEager(
+  "./components/**/*.vue"
+);
 
-// export const components = Object.fromEntries(
-//   Object.entries(componentFiles).map(([path, component]) => {
-//     return [
-//       path.split("/").slice(-1)[0].replace(".vue", ""),
-//       component.default,
-//     ];
-//   })
-// );
-
-// export const Visualia: Plugin = {
-//   install: (app) => {
-//     Object.entries(components).forEach(([name, component]) => {
-//       app.component(name, component);
-//     });
-//     Object.entries(utils).forEach(
-//       ([name, util]) => (app.config.globalProperties[name] = util)
-//     );
-//   },
-// };
-
-import VSlider from "./components/VSlider.vue";
-import VAnimate from "./components/VAnimate.vue";
+export const components = Object.fromEntries(
+  Object.entries(componentFiles).map(([path, component]) => {
+    return [
+      path.split("/").slice(-1)[0].replace(".vue", ""),
+      component.default,
+    ];
+  })
+);
 
 export const Visualia: Plugin = {
   install: (app) => {
-    const state = reactive({}) as any;
-    app.provide("state", state);
-    app.component("VSlider", VSlider);
-    app.component("VAnimate", VAnimate);
+    // Set up global state
 
+    const state = <Record<string, any>>reactive({});
+    app.provide("state", state);
+
+    // Global state utilities
     app.config.globalProperties.get = (
       key: string,
       def?: string | number | boolean
@@ -52,5 +39,17 @@ export const Visualia: Plugin = {
     ): void => {
       state[key] = value;
     };
+
+    // Load other utilities
+
+    Object.entries(utils).forEach(
+      ([name, util]) => (app.config.globalProperties[name] = util)
+    );
+
+    // Load components
+
+    Object.entries(components).forEach(([name, component]) => {
+      app.component(name, component);
+    });
   },
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,4 +3,3 @@ export * from "./colors";
 export * from "./svgpaths";
 export * from "./svgtransforms";
 export * from "./trig";
-export * from "./variables";
