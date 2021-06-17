76074262264c721845025a8cc4d9138f3f248c1b
kristjanjansen <kristjan.jansen@gmail.com>
2021-06-17 23:52:03 +0300

    mouse

--- docs/getting-started.md
+++ docs/getting-started.md
@@ -1,5 +1,7 @@
 <v-mouse style="border: 1px solid black">
-  <svg width="100" height="100" style="background: red" />
+  <svg width="100" height="100">
+    <circle v-if="!get('mouse').outside" :cx="get('mouse').x" :cy="get('mouse').y" r="10" :fill="get('mouse').pressed ? 'red' : 'black'" />
+  </svg>
 </v-mouse>
 
 {{ get("mouse") }}
--- src/components/VMouse.vue
+++ src/components/VMouse.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed, ref, useContext, watch } from "vue";
-import { useMouse, useMouseInElement } from "@vueuse/core";
+import { useMouse, useMouseInElement, useMousePressed } from "@vueuse/core";
 import { state, get } from "../utils";
 
 const { slots } = useContext();
@@ -11,22 +11,32 @@ const { slots } = useContext();
 const isSlot = computed(() => slots.default?.().length);
 
 const target = ref(null);
+
 const { x, y } = useMouse();
 const { elementX, elementY, isOutside } = useMouseInElement(target);
+const { pressed } = useMousePressed({ target });
+
 watch([x, y, elementX, elementY], () => {
   if (isSlot.value) {
     if (!isOutside.value) {
-      state.mouse = { x: elementX.value, y: elementY.value };
+      state.mouse = {
+        x: elementX.value,
+        y: elementY.value,
+        pressed,
+        outside: isOutside,
+      };
     }
   }
   if (!isSlot.value) {
-    state.mouse = { x: x.value, y: y.value };
+    state.mouse = { x: x.value, y: y.value, pressed };
   }
 });
 </script>
 
 <template>
-  <div v-if="isSlot">
-    <div ref="target" style="display: inline-flex"><slot /></div>
+  <div v-if="isSlot" style="display: grid; grid-template-columns: auto 1fr">
+    <div ref="target" style="border: 1px solid red">
+      <slot />
+    </div>
   </div>
 </template>
