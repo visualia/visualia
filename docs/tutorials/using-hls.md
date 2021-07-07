dc326952282aafb02c3bc7725709002c190c9df0
kristjanjansen <kristjan.jansen@gmail.com>
2021-07-07 09:52:44 +0300

    cleanup

--- docs/tutorials/using-hls.md
+++ docs/tutorials/using-hls.md
@@ -11,10 +11,10 @@
 </svg>
 
 ```
-<v-svg width="300" height="300">
+<v-svg width="300" height="300" padding="20">
 ```
 
-<v-svg width="300" height="300" class="shadow">
+<v-svg width="300" height="300" class="shadow" padding="10">
   <rect width="300" height="300" fill="none" stroke="black" />
   <circle :cx="150" :cy="150" :r="150" opacity="0.1" />
   <circle v-for="g in rectgrid(11,11,30)" :cx="g.x" :cy="g.y" r="3" />
--- src/components/VSvg.vue
+++ src/components/VSvg.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
 import { computed } from "vue";
 
-const props =
-  defineProps<{ width: number | string; height: number | string }>();
-
-const padding = 10;
+const props = defineProps<{
+  width: number | string;
+  height: number | string;
+  viewBox?: string;
+  padding?: number | string; // 10 by default
+}>();
 
 const size = computed(() => {
+  const padding = parseFloat(String(props.padding || 10));
   const width = parseFloat(String(props.width)) + padding * 2;
   const height = parseFloat(String(props.width)) + padding * 2;
   const viewBox = `-${padding} -${padding} ${width} ${height}`;
   const style = {
     maxWidth: `${width}px`,
-    //transform: `translate(${-padding}px,${padding}px)`,
+    // transform: `translate(${-padding}px,${-padding}px)`,
   };
   return { width, height, viewBox, style };
 });
