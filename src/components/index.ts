d3cf8a0adbf097fb70d2003c4f76be129ec3b523
kristjanjansen <kristjan.jansen@gmail.com>
2021-07-09 13:54:04 +0300

    feat: es example

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
+import { defineAsyncComponent } from "vue";
+
+export const VMath = defineAsyncComponent(() => import("./VMath.vue"));
+
 export { default as VAnimate } from "./VAnimate.vue";
-export { default as VMath } from "./VMath.vue";
 export { default as VSlider } from "./VSlider.vue";
 export { default as VSvg } from "./VSvg.vue";
