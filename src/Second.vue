f6ce8083b39c5f1295ca5a615c4ea2ab6df6f2bb
kristjanjansen <kristjan.jansen@gmail.com>
2021-08-12 10:36:23 +0300

    second

--- src/Second.vue
+++ src/Second.vue
@@ -1,6 +1,22 @@
-<script setup>
-import { ref } from "vue";
-const b = ref(0);
+<script setup lang="ts">
+import { computed } from "vue";
+const props = defineProps<{
+  value?: number;
+  modelValue?: number;
+}>();
+
+const emit = defineEmits<(e: "update:modelValue", value: number) => number>();
+
+const progress = computed({
+  get: () => props.modelValue,
+  set: (value) => {
+    emit("update:modelValue", value!);
+  },
+});
+
+emit("update:modelValue", props.modelValue || props.value || 0);
 </script>
 
-<template><input type="range" v-model="b" /> b: {{ b }} ></template>
+<template>
+  <input type="range" v-model.number="progress" /> "{{ progress }}"
+</template>
