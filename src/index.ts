db80fe69aff19c79c943284b48f1d89232674729
kristjanjansen <kristjan.jansen@gmail.com>
2021-08-12 16:46:12 +0300

    r10

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,28 @@ export const Visualia: Plugin = {
   },
 };
 
+/*
+
+import { defineAsyncComponent, Plugin } from "vue";
+
+export const components = import.meta.glob("./components/*.vue");
+
+import * as utils from "./utils";
+export * from "./utils";
+
+export const Visualia: Plugin = {
+  install: (app) => {
+    Object.entries(components).forEach(([path, component]) => {
+      const name = path.split("/").slice(-1)[0].replace(".vue", "");
+      app.component(name, defineAsyncComponent(component));
+    });
+    Object.entries(utils).forEach(
+      ([name, util]) => (app.config.globalProperties[name] = util)
+    );
+  },
+};
+*/
+
 // import { reactive } from "vue";
 
 // export { default as First } from "./First.vue";
