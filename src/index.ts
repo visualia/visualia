5f858f43fdce2def643e0dd8996401530cf81bc0
kristjanjansen <kristjan.jansen@gmail.com>
2021-08-12 10:58:07 +0300

    new take

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
-// import { Plugin, ComponentOptions } from "vue";
-// import * as components from "./components";
-// import * as utils from "./utils";
+import { Plugin, ComponentOptions } from "vue";
+import * as components from "./components";
+import * as utils from "./utils";
 
-import { reactive } from "vue";
+export * from "./components";
+export * from "./utils";
+export { components, utils };
 
-// export * from "./components";
-// export * from "./utils";
-// export { components, utils };
+export const Visualia: Plugin = {
+  install: (app) => {
+    Object.entries(components).forEach(([name, component]) => {
+      app.component(name, component as ComponentOptions);
+    });
+    Object.entries(utils).forEach(
+      ([name, util]) => (app.config.globalProperties[name] = util)
+    );
+  },
+};
 
-// export const Visualia: Plugin = {
-//   install: (app) => {
-//     Object.entries(components).forEach(([name, component]) => {
-//       app.component(name, component as ComponentOptions);
-//     });
-//     Object.entries(utils).forEach(
-//       ([name, util]) => (app.config.globalProperties[name] = util)
-//     );
-//   },
-// };
+// import { reactive } from "vue";
 
-export { default as First } from "./First.vue";
-export { default as Second } from "./Second.vue";
+// export { default as First } from "./First.vue";
+// export { default as Second } from "./Second.vue";
 
-export const ab = reactive({ a: 0, b: 0 });
+// export const ab = reactive({ a: 0, b: 0 });
